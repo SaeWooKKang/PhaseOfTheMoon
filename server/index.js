1c396780b0f3ada1e3859475033b307caba09d59
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-03-07 20:39:31 +0900

    주석 삭제

--- server/index.js
+++ server/index.js
@@ -3,34 +3,30 @@ const app = express();
 const axios = require('axios');
 const fs = require('../client/src/fs');
 
-const getAPI = async (params) => {
+const getAPI = async params => {
   const instance = axios.create({
     baseURL : 'http://apis.data.go.kr/B090041/openapi/service/'
-  })
-  
-  let response;
+  });
 
-  // try {
-      response = await instance(params);
-  // } catch (e) {
-  //   console.log('뭐냐-----------------');
-  // }
+  let response = await instance(params);
+
   return response;
-}
+};
 
 const getLunday = async () => {
   const {year, month, date} = fs.date();
-  const path = `LunPhInfoService/getLunPhInfo?`;
-  const query = `serviceKey=NKb3XjlkqjPQG%2BBxVkHZkue02%2FZDYlwPGSBlx4zjEx8fTaVoAzdHSjrEVNCTS%2BCmGQCPDKXymHxqRCFgwC%2Bc5w%3D%3D&solYear=${year}&solMonth=${month}&solDay=${date}`;
+  const path = `LunPhInfoService/getLunPhInfo`;
+  const query = `?serviceKey=NKb3XjlkqjPQG%2BBxVkHZkue02%2FZDYlwPGSBlx4zjEx8fTaVoAzdHSjrEVNCTS%2BCmGQCPDKXymHxqRCFgwC%2Bc5w%3D%3D&solYear=${year}&solMonth=${month}&solDay=${date}`;
 
   const response = getAPI(path + query );
   
   return response;
 };
+
 const getLunCycle = async () => {
   const {YMD} = fs.date();
-  const path = `RiseSetInfoService/getAreaRiseSetInfo?`;
-  const query = `serviceKey=NKb3XjlkqjPQG%2BBxVkHZkue02%2FZDYlwPGSBlx4zjEx8fTaVoAzdHSjrEVNCTS%2BCmGQCPDKXymHxqRCFgwC%2Bc5w%3D%3D&locdate=${YMD}&location=${encodeURI('서울')}`;
+  const path = `RiseSetInfoService/getAreaRiseSetInfo`;
+  const query = `?serviceKey=NKb3XjlkqjPQG%2BBxVkHZkue02%2FZDYlwPGSBlx4zjEx8fTaVoAzdHSjrEVNCTS%2BCmGQCPDKXymHxqRCFgwC%2Bc5w%3D%3D&locdate=${YMD}&location=${encodeURI('서울')}`;
 
   const response = getAPI(path + query);
 
@@ -44,6 +40,7 @@ app.get("/lun/day", (req,res) => {
       res.json(response.data.response.body);
     });
   });
+
 app.get("/lun/cycle", (req,res) => {
   getLunCycle()
     .then(response => {
