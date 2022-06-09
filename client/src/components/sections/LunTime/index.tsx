8eb85acd929f331bdc8dcb6843eb0c01c090df27
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-06-09 16:47:23 +0900

    refactor: 컴포넌트별로 뜨던 loading을 하나로 통합
    - 하위 컴포넌트에서 각각 api 요청-> 상위 컴포넌트로 Today에서 api요청
    - 모든 api 요청 완료시 화면 렌더링
    
    Close #25

--- client/src/components/sections/LunTime/index.tsx
+++ client/src/components/sections/LunTime/index.tsx
@@ -1,44 +1,35 @@
-import React, { useEffect } from "react";
+import React from "react";
 
-import { lunCycle } from "../../../redux/actions/lunCycleAction";
-import { useAppSelector, useAppDispath } from '../../../redux/hooks';
+import { useAppSelector } from '../../../redux/hooks';
 
 import { LunTimeWrapper, CycleWrapper } from './style';
 
 const LunTime = () => {
-  const dispatch = useAppDispath();
-  const { data, isLoading} = useAppSelector(state => state.lun.cycle);
+  const { data } = useAppSelector(state => state.lun.cycle);
   const canISeeTheMoon = useAppSelector(({ lun: { canISeeTheMoon } })=> canISeeTheMoon);
-
-  useEffect(() => { dispatch(lunCycle()) }, []);
   
   return (
     <LunTimeWrapper>
-      { isLoading 
-        ? <div className='loading-ment'>Loading...</div>
-        : ( 
-          <>
-            <div className='cnt-rise-transit-set'>
-              <div className='cnt-items-rise-transit-set'>
-                { data
-                   && 
-                    <CycleWrapper flexDirection={canISeeTheMoon}>
-                      <div className='item-rise-transit-set'>월출 {data.moonrise}</div>
-                      <div className='item-rise-transit-set'>월중 {data.moontransit}</div>
-                      <div className='item-rise-transit-set'>월몰 {data.moonset}</div>
-                    </CycleWrapper> 
-                }
-              </div>
-            </div>
-
+      <>
+        <div className='cnt-rise-transit-set'>
+          <div className='cnt-items-rise-transit-set'>
             { 
-              canISeeTheMoon ||
-                <div className='ment'>
-                  현재 시각은 달을 볼 수 없습니다.
-                </div>
-            } 
-          </>
-          )}
+                <CycleWrapper flexDirection={canISeeTheMoon}>
+                  <div className='item-rise-transit-set'>월출 { data!.moonrise }</div>
+                  <div className='item-rise-transit-set'>월중 { data!.moontransit }</div>
+                  <div className='item-rise-transit-set'>월몰 { data!.moonset }</div>
+                </CycleWrapper> 
+            }
+          </div>
+        </div>
+
+        { 
+          canISeeTheMoon ||
+            <div className='ment'>
+              현재 시각은 달을 볼 수 없습니다.
+            </div>
+        } 
+      </>
     </LunTimeWrapper>)
 };
 
